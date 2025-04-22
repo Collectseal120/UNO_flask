49ddf5f0c855b6c4062e8d959af90b4ad430a57c
Collectseal120 <140808649+Collectseal120@users.noreply.github.com>
2025-04-22 21:09:17 +0300

    uncommented monkey_patch

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ class Room:
 #        Flask Setup          #
 # --------------------------- #
 
-#eventlet.monkey_patch()
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
